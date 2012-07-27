e46b7e15725ea9b76936653771f4f218e9aafa2f
András Kovács <puttamalac@gmail.com>
2012-07-27 12:49:19 +0200

    stat renderings

--- tetris.py
+++ tetris.py
@@ -4,42 +4,115 @@ import os
 import random
 
 
+#TODO
+	# Simplify printing of right text using piece images
+	# piece stats render
+	# score update and render
+	# menu
+	# phantom mode?
 
 
-
-
-
-FPS_LIMIT = 60 #level speeds are calibrated to 60 fps!
+#constants
+LEVEL_SPEEDS = (30, 24, 19, 15, 12, 10, 8, 6, 5, 4)
+FPS_LIMIT = 60 #level speeds are calibrated for 60 fps!
 SCREEN_WIDTH = 40
 SCREEN_HEIGHT = 25
 FIELD_WIDTH = 12
 FIELD_HEIGHT = 21
 
 
+#pieces
+class O(object):
+	image		= " xx\n xx".replace('x', chr(219))
+	initpos 	= ((5, 0), (6, 0), (5, 1), (6, 1))
+	color  	 	= lt.blue
+	rotations 	= [((0, 0), (0, 0), (0, 0), (0, 0))]
+
+class I(object):
+	image		= "xxxx".replace('x', chr(219))
+	initpos 	= ((4, 0), (5, 0), (6, 0), (7, 0))
+	color  	 	= lt.red
+	rotations 	= [(( 2, -1), ( 1, 0), (0,  1), (-1,  2)), 
+				   ((-2,  1), (-1, 0), (0, -1), ( 1, -2))]
+
+class T(object):
+	image		= " xxx\n  x".replace('x', chr(219))
+	initpos 	= ((5, 0), (6, 0), (7, 0), (6, 1))
+	color  	 	= lt.orange
+	rotations 	=  [(( 1, 1), (0, 0), (-1, -1), ( 1, -1)),
+					(( 1,-1), (0, 0), (-1,  1), (-1, -1)),
+					((-1,-1), (0, 0), ( 1,  1), ( -1, 1)),
+					((-1, 1), (0, 0), ( 1, -1), ( 1,  1))]
+
+class L(object):
+	image		= " xxx\n x".replace('x', chr(219))
+	initpos 	= ((5, 0), (6, 0), (7, 0), (5, 1))
+	color  	 	= lt.purple
+	rotations 	=  [(( 1, 1), (0, 0), (-1, -1), ( 2,  0)),
+					(( 1,-1), (0, 0), (-1,  1), ( 0, -2)),
+					((-1,-1), (0, 0), ( 1,  1), (-2,  0)),
+					((-1, 1), (0, 0), ( 1, -1), ( 0,  2))]
+		
+class J(object):
+	image		= " xxx\n  x".replace('x', chr(219))
+	initpos 	= ((5, 0), (6, 0), (7, 0), (7, 1))
+	color  	 	= lt.silver
+	rotations 	=  [(( 1, 1), (0, 0), (-1, -1), ( 0, -2)),
+					(( 1,-1), (0, 0), (-1,  1), (-2,  0)),
+					((-1,-1), (0, 0), ( 1,  1), ( 0,  2)),
+					((-1, 1), (0, 0), ( 1, -1), ( 2,  0))]
+
+class S(object):
+	image		= " xx\n xx".replace('x', chr(219))
+	initpos 	= ((5, 1), (6, 1), (6, 0), (7, 0))
+	color  	 	= lt.green
+	rotations 	=  [(( 2,  0), ( 1, -1), (0, 0), ( -1, -1)),
+					((-2,  0), (-1,  1), (0, 0), (  1,  1))]
+
+class Z(object):
+	image		= " xx\n  xx".replace('x', chr(219))
+	initpos 	= ((5, 0), (6, 0), (6, 1), (7, 1))
+	color  	 	= lt.cyan
+	rotations 	=  [(( 1,  1), (0, 0), ( 1, -1), (0, -2)),
+					((-1, -1), (0, 0), (-1,  1), (0,  2))]
+
+
+#variables
+show_next = True
+level = 0
+full_lines = 0
+score = 0
+curr_piece = random.choice((O, I, T, L, J, S, Z))
+next_piece = random.choice((O, I, T, L, J, S, Z))
+move_speed = LEVEL_SPEEDS[level]
+piece_stats = {L:0, I:0, T:0, S:0, Z:0, O:0, J:0}
+stats_sum = 0
+
+
+#coloring helper
 def help_color_rect_foreground(con, a, b, c, d, color):
 	for i in xrange(a, a+c):
 		for j in xrange(b, b+d):
 			lt.console_set_char_foreground(con, i, j, color)
 
 
+#initializing windows
 lt.console_set_custom_font(os.path.join('data', 'fonts','terminal16x16_gs_ro.png'), lt.FONT_TYPE_GREYSCALE | lt.FONT_LAYOUT_ASCII_INROW)
 lt.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'KuttaTetris', fullscreen=False)
-
 lt.sys_set_fps(FPS_LIMIT)
-
 playfield_bg = lt.console_new(FIELD_WIDTH, FIELD_HEIGHT)
 playfield    = lt.console_new(FIELD_WIDTH, FIELD_HEIGHT)
 
-######################## render playfield_bg ########################
 
+#render playfield_bg 
 lt.console_set_default_foreground(playfield_bg, lt.light_blue)
 for i in xrange(0, FIELD_HEIGHT-1):
 	lt.console_print(playfield_bg, 0, i, chr(178) + ' . . . . .'  + chr(178))
 lt.console_print(playfield_bg, 0, FIELD_HEIGHT-1, chr(178) * FIELD_WIDTH)
 help_color_rect_foreground(playfield_bg, 1, 0, FIELD_WIDTH-2, FIELD_HEIGHT-1, lt.blue)
 
-################## initialize non-changing window elements ###########
 
+#render non-changing window elements 
 left_text = """
 Your level: 
 Full lines:
@@ -92,8 +165,6 @@ right_text = """
 lt.console_print(0, 0, 0, left_text)
 lt.console_print(0, 26, 0, right_text)
 
-################## coloring ###################################
-
 for i in xrange(14, 27):
 	lt.console_set_char_foreground(0, i, 23, lt.red)
 
@@ -102,83 +173,47 @@ for i,color in zip(xrange(5, 20, 2), stat_colors):
 	help_color_rect_foreground(0, 28, i, 8, 2, color)
 
 
-################# playfield mechanics ###########################
+# render variables
+def render_level():
+	lt.console_print(0, 12, 1, str(level))
+
+def render_full_lines():
+	lt.console_print(0, 11, 2, str(full_lines).center(3))
+
+def render_score():
+	lt.console_set_default_foreground(0, lt.yellow)
+	lt.console_print(0, 8, 4, str(score).rjust(4))
+	lt.console_set_default_foreground(0, lt.white)
+
+def render_next():
+	lt.console_set_default_foreground(0, next_piece.color)
+	lt.console_print(0, 4, 21, '    \n    ')
+	lt.console_print(0, 4, 21, next_piece.image)
+	lt.console_set_default_foreground(0, lt.white)
+
+def render_stats_sum():
+	lt.console_print(0, 37, 20, str(stats_sum).rjust(3))
+
+render_level()
+render_full_lines()
+render_score()
+render_next()
+render_stats_sum()
 
 
+#playfield mechanics 
 is_blocked = [[True]*FIELD_HEIGHT] + [[False]*(FIELD_HEIGHT-1) + [True] for i in xrange(FIELD_WIDTH-2)] + [[True]*FIELD_HEIGHT]
 cell_color = [[False] * FIELD_HEIGHT for i in xrange(FIELD_WIDTH)]
 		
 
-class O(object):
-	initpos 	= ((5, 0), (6, 0), (5, 1), (6, 1))
-	color  	 	= lt.blue
-	rotations 	= [((0, 0), (0, 0), (0, 0), (0, 0))]
-
-class I(object):
-	initpos 	= ((4, 0), (5, 0), (6, 0), (7, 0))
-	color  	 	= lt.red
-	rotations 	= [(( 2, -1), ( 1, 0), (0,  1), (-1,  2)), 
-				   ((-2,  1), (-1, 0), (0, -1), ( 1, -2))]
-
-class T(object):
-	initpos 	= ((5, 0), (6, 0), (7, 0), (6, 1))
-	color  	 	= lt.orange
-	rotations 	=  [(( 1, 1), (0, 0), (-1, -1), ( 1, -1)),
-					(( 1,-1), (0, 0), (-1,  1), (-1, -1)),
-					((-1,-1), (0, 0), ( 1,  1), ( -1, 1)),
-					((-1, 1), (0, 0), ( 1, -1), ( 1,  1))]
-
-class L(object):
-	initpos 	= ((5, 0), (6, 0), (7, 0), (5, 1))
-	color  	 	= lt.purple
-	rotations 	=  [(( 1, 1), (0, 0), (-1, -1), ( 2,  0)),
-					(( 1,-1), (0, 0), (-1,  1), ( 0, -2)),
-					((-1,-1), (0, 0), ( 1,  1), (-2,  0)),
-					((-1, 1), (0, 0), ( 1, -1), ( 0,  2))]
-		
-class J(object):
-	initpos 	= ((5, 0), (6, 0), (7, 0), (7, 1))
-	color  	 	= lt.silver
-	rotations 	=  [(( 1, 1), (0, 0), (-1, -1), ( 0, -2)),
-					(( 1,-1), (0, 0), (-1,  1), (-2,  0)),
-					((-1,-1), (0, 0), ( 1,  1), ( 0,  2)),
-					((-1, 1), (0, 0), ( 1, -1), ( 2,  0))]
-
-class S(object):
-	initpos 	= ((5, 1), (6, 1), (6, 0), (7, 0))
-	color  	 	= lt.green
-	rotations 	=  [(( 2,  0), ( 1, -1), (0, 0), ( -1, -1)),
-					((-2,  0), (-1,  1), (0, 0), (  1,  1))]
-
-class Z(object):
-	initpos 	= ((5, 0), (6, 0), (6, 1), (7, 1))
-	color  	 	= lt.cyan
-	rotations 	=  [(( 1,  1), (0, 0), ( 1, -1), (0, -2)),
-					((-1, -1), (0, 0), (-1,  1), (0,  2))]
-
-	
-
-
-
-#(30, 24, 19, 15, 12, 10, 8, 6, 5, 4)
-
-MOVE_SPEED = 30
-
-
-
-
 def drop_piece(piece):
 
-	global pos, rotation_state
-
+	global pos, rotation_state, stats_sum
 	rotation_state = 0
 	pos = piece.initpos[:]
 
-
 	def handle_keys():
-		global pos, move_count, rotation_state
-
-		# handle keys
+		global pos, move_count, rotation_state, level, move_speed
 
 		key = lt.console_check_for_keypress(True) # exit
 		if key.vk == lt.KEY_ESCAPE:
@@ -207,7 +242,12 @@ def drop_piece(piece):
 				if rotation_state == len(piece.rotations):
 					rotation_state = 0
 
-		#render
+		elif key.vk == lt.KEY_6: #increase level
+			if level != len (LEVEL_SPEEDS) - 1:
+				level += 1
+				move_speed = LEVEL_SPEEDS[level]
+				render_level()
+
 
 		lt.console_blit(playfield_bg, 0, 0, FIELD_WIDTH, FIELD_HEIGHT, playfield, 0, 0)
 
@@ -225,8 +265,10 @@ def drop_piece(piece):
 		lt.console_flush()
 
 	def update_lines():
+		global full_lines
 		for h in xrange(FIELD_HEIGHT-1):
 			if all(is_blocked[w][h] for w in xrange(1, FIELD_WIDTH-1)):
+				full_lines += 1
 				for w in xrange(1, FIELD_WIDTH-1):
 					is_blocked[w][h] = False
 					cell_color[w][h] = False
@@ -234,39 +276,38 @@ def drop_piece(piece):
 					for wn in xrange(1, FIELD_WIDTH-1):
 						is_blocked[wn][hn], is_blocked[wn][hn+1] =  is_blocked[wn][hn+1], is_blocked[wn][hn]
 						cell_color[wn][hn], cell_color[wn][hn+1] =  cell_color[wn][hn+1], cell_color[wn][hn]
-
+		render_full_lines()
 	
 	if any(is_blocked[x][y] for x, y in pos):
 		return "fail"  #game is over if initial position is already filled
-
-	#wait quarter sec at top regardless of speed (seems to be the case in Tetris 3.12)
-	for i in xrange(FPS_LIMIT/4):
+	
+	for i in xrange(FPS_LIMIT/4): #wait quarter sec at top regardless of speed (seems to be the case in Tetris 3.12)
 		if handle_keys() == "exit": return "exit"
 
 	global move_count
-	move_count = MOVE_SPEED
+	move_count = move_speed
 	while True:
 		move_count -= 1
 		if not move_count:
-			move_count = MOVE_SPEED
+			move_count = move_speed
 			npos = tuple((x, y+1) for x, y in pos)
 			if any(is_blocked[x][y] for x, y in npos):
 				for x, y in pos:
 					is_blocked[x][y] = True
 					cell_color[x][y] = piece.color
+
+				piece_stats[curr_piece] += 1
+				stats_sum += 1
 				update_lines()
+				render_stats_sum() 
 				return "newdrop"
 			else:
 				pos = npos
-
 		if handle_keys() == "exit": return "exit"
 
 
-
-curr_piece = random.choice((O, I, T, L, J, S, Z))
+#main loop
 while True:
-
-	next_piece = random.choice((O, I, T, L, J, S, Z))
 	action = drop_piece(curr_piece)
 	if action == "exit":
 		break
@@ -276,6 +317,8 @@ while True:
 		pass
 
 	curr_piece = next_piece 
+	next_piece = random.choice((O, I, T, L, J, S, Z))
+	render_next()
 
 
 
